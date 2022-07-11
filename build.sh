7a92e3e157da87393ccc3144735a8081e60b37d5
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-11 16:14:49 +0200

    Upgraded build and run scripts, updated conf files.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-docker rm  $(docker stop $(docker ps -a -q --filter ancestor="slurm_db_daemon" --format="{{.ID}}"& )& ) &
-docker image rm -f slurm_db_daemon &
+echo -e "\033[0;31m"
+docker rm  $(docker stop $(docker ps -a -q --filter ancestor="slurm_db_daemon" --format="{{.ID}}"))
+docker image rm -f slurm_db_daemon
+echo -e "\033[0;32m###########_Building image_###########\033[0;0m"
 docker build -t slurm_db_daemon .
